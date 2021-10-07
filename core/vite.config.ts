8527afcdb36fdf7fe7c2332a1aa00f8574cbefe8
Pervez Iqbal <pervezfunctor@gmail.com>
2021-10-07 22:31:10 +0530

    initial common library 'core'

---
+++ core/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'MyLib',
+      fileName: (format) => `my-lib.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React'
+        }
+      }
+    }
+  }
+})
+
